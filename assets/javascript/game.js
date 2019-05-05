e92a4cbb5034c18673fe6183ef187360582545ee
Brookee <oliver.brooke.a@gmail.com>
2019-05-05 14:37:32 -0700

    game

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var losses = 0; 
+var wins = 0;
+var guesses = 10;
+var guessessofar = [];
+var psyhicGuess;
