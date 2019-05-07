f4a6945c5c3130cfb2e129610dffab5207d887e9
Brookee <oliver.brooke.a@gmail.com>
2019-05-06 22:25:27 -0700

    game updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,28 @@ var guesses = 10;
 var guessessofar = [];
 var psyhicGuess;
 
-var userGuess = letters[Math.floor(Math.random() * letters.length)];
+document.onkeyup = function(event){
+    var userChoice = event.key;
 
-            // document.onkeyup = function(event)
-        // }   
+    var userGuess = letters[Math.floor(Math.random() * letters.length)];
+
+    var answers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
+    if (answers.indexOf(userChoice) > -1 {
+        if (userChoice === userGuess) {
+            wins++;
+            guesses = 10;
+            psyhicGuess = [];
+        }
+    }
+
+    
             // var userChoice = " You guessed correct! ";
             // var wrongChoice = " Bummer! You guessed wrong ";
 
             // if userChoice {
 
-        // }
+        }
 
 //var lettersfar = function() {
     //document.getElementById()//
