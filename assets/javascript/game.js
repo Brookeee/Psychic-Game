46f44dacf867b151faf51dd68adfac3a7293f17c
Brookee <oliver.brooke.a@gmail.com>
2019-05-08 11:57:02 -0700

    wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,39 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var losses = 0; 
 var wins = 0;
-var guesses = 10;
-var guessessofar = [];
+var guesses = [];
+var guessesLeft = 10;
+var guessesSoFar = []; // capture user input
 var psyhicGuess;
 
 document.onkeyup = function(event){
     var userChoice = event.key;
 
-    var userGuess = letters[Math.floor(Math.random() * letters.length)];
-
-    var answers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    guessesSoFar.push(userChoice);
+    var psyhicResult = letters[Math.floor(Math.random() * letters.length)];
     
-    if (answers.indexOf(userChoice) > -1 {
-        if (userChoice === userGuess) {
-            wins++;
-            guesses = 10;
-            psyhicGuess = [];
-        }
+    if (userChoice === psyhicResult) {
+        wins++;
+        guesses;
     }
-
-    
-            // var userChoice = " You guessed correct! ";
-            // var wrongChoice = " Bummer! You guessed wrong ";
-
-            // if userChoice {
-
-        }
-
-//var lettersfar = function() {
-    //document.getElementById()//
-//
+    else {
+        guesses--;
+    }
+     if (guesses === 0) {
+         losses++;
+         guessesLeft -= 10;
+     }
+     document.getElementById('wins').innerHTML = "wins: " + wins;
+     document.getElementById('losses').innerHTML = "losses: " + losses;
+    //  document.getElementById('guesses').innerHTML = "Guesses : " + guesses;
+     //document.getElementById('left').innerHTML = "Guesses Left: " + guessesLeft;
+    }
+    // function printKey(letters) {
+        // if (guessesLeft == 10) {
+            // document.getElementById("guesses").innerHTML += letters;
+        
+        // }
+        // else {
+            // document.getElementById("guesses").innerHTML =+ ", " + letters;
+        // }
+    // }
