6691fd1d7eafc8ce112f2998a26d875c87681ce9
Brookee <oliver.brooke.a@gmail.com>
2019-05-11 12:03:24 -0700

    draft

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ Rules:
 1. Think of a letter
 2. Enter in letter you guess I'm thinking of 
 3. See if you get the correct letter
-4. See if you can guess correctly 3 times in a row
+4. Try not to lose! 
+
+Instuctions 
+
+You're going to make a game just like the one in the video. Essentially, the app randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
+
+* Guess what letter I'm thinking of
+* Wins: (# of times the user has guessed the letter correctly)
+* Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+* Guesses Left: (# of guesses left. This will update)
+* Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+* When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+
 
 Good luck! 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,49 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var losses = 0; 
-var wins = 0;
+var loss = 0; 
+var win = 0;
 var guessesLeft = 10;
-var guessesSoFar = [""]; // capture user input
-var psychicGuess;
+var guessesSoFar = []; // capture user input
+var psychicGuess = psychicGuess;
+
+function compGuess() { //computer letter choice
+    psychicGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log(psychicGuess);
+}
 
   document.onkeypress = function(event) {
     var userChoice = event.key;
 
-    // guessesSoFar.push(userChoice);
-    var psychicGuess = letters[Math.floor(Math.random() * letters.length)];
-
-    if (userChoice === psychicGuess) {
-      wins++;
-      guessesLeft = 10;
-      guessesSoFar = [""];
     // if user's key is correct, wins goes up by 1
-    } else if (guessesLeft == 0) {
-      losses++;
+    if (userChoice === psychicGuess) {
+      win++;
       guessesLeft = 10;
-      guessesSoFar = [""];
-      // if users key is incorrect, losses go up by 1
-
-    } if (userChoice !== psychicGuess) {
-      guessesLeft--;
-      // guesses left decrementing
+      guessesSoFar = [];
+      alert("We have a winner!");
     }
-  }
-    document.getElementById("wins").innerHTML + wins;
-    document.getElementById("losses").innerHTML + losses;
-    document.getElementById("left").innerHTML + guessesLeft;
-    document.getElementById("guesses").innerHTML + guessesSoFar;
+    // reset game/score after user wins, without reloading page.. 
+
+    compGuess(); // guesses left decrementing when random word not correct
+    if (userChoice !== psychicGuess) {
+      guessesLeft--;
+    }
+    // if user's key incorrect, lossses go up by 1
+    if (guessesLeft == 0) {
+        loss++;
+        guessesSoFar = []
+        guessesLeft = 10;
+        alert("Oh no! You lost. Try again!");
+    }
+    // reset game/score after user losses, without reloading page ..
+    if (guessesSoFar.indexOf(userChoice) >=0) {
+
+    } else { // log user's input for guessed letters
+        guessesSoFar.push(userChoice);
+        document.getElementById("playerGuess").innerHTML = guessesSoFar;
+        console.log(guessesSoFar);
+    }
+
+      // log results
+    document.getElementById("wins").textContent = win;
+    document.getElementById("losses").textContent = loss;
+    document.getElementById("guess").textContent = guessesLeft;
+}
